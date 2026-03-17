766fada56d1d5bce499496db7f9451e75b5c92ab
abutalha08 <taufique752@gmail.com>
2026-03-17 16:53:01 +0600

    fetch_api_17

---
+++ fetch_api_17.js
@@ -0,0 +1,7 @@
+fetch('https://jsonplaceholder.typicode.com/users')
+  .then(response => response.json())
+  .then(data => {
+    data.forEach(user => {
+      console.log(user.name);
+    });
+  });
