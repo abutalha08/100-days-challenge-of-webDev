53b4dc484790e82524226419292527b8cbe33a22
abutalha08 <taufique752@gmail.com>
2026-03-19 13:04:31 +0600

    async_await_in_js_19

---
+++ async_await_in_js_19.js
@@ -0,0 +1,10 @@
+async function loadUsers() {
+  const response = await fetch('https://jsonplaceholder.typicode.com/users');
+  const data = await response.json();
+
+  data.forEach(user => {
+    console.log(user.name);
+  });
+}
+
+loadUsers();
